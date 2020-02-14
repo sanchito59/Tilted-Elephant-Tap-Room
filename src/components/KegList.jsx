34b299aa37a1ea69bdd6c8d7bc00cd2ed03b6176
Chris Sanchez <c.sanch7@gmail.com>
2020-02-14 08:53:09 -0800

    change KegList to map kegs

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -3,16 +3,28 @@ import Header from "./Header";
 import Keg from "./Keg";
 
 export default function KegList() {
+  const availableKegs = [
+    {
+      name: "African Amber",
+      brand: "Mac & Jack's",
+      price: "5.49",
+      alcoholContent: "6.7%",
+      description:
+        "This beer erupts with a floral, hoppy taste, followed by a well rounded malty middle, finishing with a nicely organic hop flavor."
+    }
+  ];
   return (
     <div>
       <Header />
-      <Keg
-        name="Amber Jack"
-        brand="Mac and Jack"
-        price="5.49"
-        alcoholContent="6.7%"
-        description="Warm, nutty, oaken"
-      />
+      {availableKegs.map((keg, index) => (
+        <Keg
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          alcoholContent={keg.alcoholContent}
+          description={keg.description}
+        />
+      ))}
     </div>
   );
 }
