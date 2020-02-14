cd1e8eb26048ccaade7ba055cbf3bbf665586145
Chris Sanchez <c.sanch7@gmail.com>
2020-02-14 09:11:19 -0800

    update props, style Keg

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -2,13 +2,43 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function Keg(props) {
+  const keg = {
+    padding: "16px"
+  };
+
+  const brand = {
+    fontStyle: "italic"
+  };
+
+  const bold = {
+    fontWeight: "bold"
+  };
+
+  const description = {
+    width: "60%"
+  };
+
+  const hr = {
+    marginTop: "12px",
+    width: "75%"
+  };
+
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h3>{props.brand}</h3>
-      <h3>{props.price}</h3>
-      <p>{props.alcoholContent}</p>
-      <p>{props.description}</p>
+    <div style={keg}>
+      <h2>{props.name}</h2>
+      <h4>
+        Made By <span style={brand}>{props.brand}</span>
+      </h4>
+      <h4>${props.price} per pint</h4>
+      <p>
+        <span style={bold}>ABV: </span>
+        {props.alcoholContent}%
+      </p>
+      <p>
+        <span style={bold}>IBU: </span> {props.IBU}
+      </p>
+      <p style={description}>{props.description}</p>
+      <hr style={hr}></hr>
     </div>
   );
 }
@@ -18,5 +48,6 @@ Keg.propTypes = {
   brand: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
   alcoholContent: PropTypes.string.isRequired,
+  IBU: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired
 };
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -8,9 +8,19 @@ export default function KegList() {
       name: "African Amber",
       brand: "Mac & Jack's",
       price: "5.49",
-      alcoholContent: "6.7%",
+      alcoholContent: "6.7",
+      IBU: "30",
       description:
         "This beer erupts with a floral, hoppy taste, followed by a well rounded malty middle, finishing with a nicely organic hop flavor."
+    },
+    {
+      name: "Ruby",
+      brand: "McMenamins",
+      price: "4.50",
+      alcoholContent: "4.13",
+      IBU: "5",
+      description:
+        "An ale that's light, crisp and refreshingly fruity. Great Western Premium 2-Row and 42 pounds of Oregon-grown raspberry puree is used to craft every colorful batch."
     }
   ];
   return (
@@ -22,6 +32,7 @@ export default function KegList() {
           brand={keg.brand}
           price={keg.price}
           alcoholContent={keg.alcoholContent}
+          IBU={keg.IBU}
           description={keg.description}
         />
       ))}
