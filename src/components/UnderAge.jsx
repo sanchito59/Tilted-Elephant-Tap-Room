d10bded7c0dbda01b51a899c23bc002a778fc606
Chris Sanchez <c.sanch7@gmail.com>
2020-02-14 13:30:20 -0800

    style Underage component

--- src/components/UnderAge.jsx
+++ src/components/UnderAge.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
 export default function Underage() {
+  const underagePage = {
+    backgroundColor: "rgb(204,224,224)",
+    height: "100vh"
+  };
+
+  const pageContent = {
+    textAlign: "center",
+    padding: "16px",
+    backgroundColor: "rgba(51, 68, 85, 0.9)",
+    color: "rgb(255, 255, 245)",
+    marginLeft: "20px",
+    marginRight: "20px",
+    borderRadius: "8px"
+  };
+
   return (
-    <div>
-      <h1>Sorry, come back when you are of legal drinking age.</h1>
+    <div style={underagePage}>
+      <div style={pageContent}>
+        <h1>Sorry, come back when you are of legal drinking age.</h1>
+      </div>
     </div>
   );
 }
